bf617a38c6fa34f941fcec310f00a5f9ffd270d3
Dmitry <dpgomberg@gmail.com>
2021-02-12 21:46:21 +0300

    jninmk

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,26 @@ $(document).ready(function(){
        $('.main-menu__part-left,.main-menu__part-right').toggleClass('active');
        $('body').toggleClass('lock');
    });
+   $(document).ready(function() {
+      $('.works').magnificPopup({
+         delegate: 'a',
+         type: 'image',
+         tLoading: 'Loading image #%curr%...',
+         mainClass: 'mfp-img-mobile',
+         gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+         },
+         image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            
+         }
+      });
+   });
 });
+
+
 
 const swiper = new Swiper('.swiper-container', {
  
